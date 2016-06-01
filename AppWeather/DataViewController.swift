615662fe315d7696acb960a360187f520637f5ed
Remi <bonnet.rem@gmail.com>
2016-06-01 14:25:04 +0200

    Initial Commit

---
+++ AppWeather/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  AppWeather
+//
+//  Created by Bonnet Rémi on 01/06/2016.
+//  Copyright © 2016 Dream Team. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
