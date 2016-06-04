46e71528f0ac16c049234e18d1047da5d335a31e
baptistelambert <bapthf2@gmail.com>
2016-06-04 16:13:04 +0200

    Created WeatherService + Weather model

--- AppWeather/Classes/Model/Weather.swift
+++ AppWeather/Classes/Model/Weather.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct Weather {
+    let cityName: String
+    let description: String
+    let temp: Double
+    let icon: String
+    let windSpeed: Double
+    let humidity: Double
+    let sunrise: Double
+    let sunset: Double
+    let pressure: Double
+    
+    init(cityName: String, description: String, temp: Double, icon: String, windSpeed: Double, humidity: Double, sunrise: Double, sunset: Double, pressure: Double) {
+        self.cityName = cityName
+        self.description = description
+        self.temp = temp
+        self.icon = icon
+        self.windSpeed = windSpeed
+        self.humidity = humidity
+        self.sunrise = sunrise
+        self.sunset = sunset
+        self.pressure = pressure
+    }
+}
--- AppWeather/Classes/Network/UrlBuilder.swift
+++ AppWeather/Classes/Network/UrlBuilder.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+class UrlBuilder {
+    
+    static func getCurrentCityWeather(city: String) -> String {
+        
+        let apiKey = "4c2cc24b24049e5d76752e73d0791a72"
+        let cityEscaped = city.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLHostAllowedCharacterSet())
+        
+        let url = "http://api.openweathermap.org/data/2.5/weather?q=\(cityEscaped)&appid=\(apiKey)"
+        
+        return url
+        
+    }
+    
+}
