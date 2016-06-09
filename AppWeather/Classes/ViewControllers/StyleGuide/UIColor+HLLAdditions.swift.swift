82545cd916ed18c0ef4e0b49f53516c17e053e74
Remi <bonnet.rem@gmail.com>
2016-06-09 15:16:22 +0200

    add UIColor+HLLAdditions

---
+++ AppWeather/Classes/ViewControllers/StyleGuide/UIColor+HLLAdditions.swift.swift
@@ -0,0 +1,69 @@
+//
+//  UIColor+HLLAdditions.swift
+//  Hello
+//
+//  Generated on Zeplin. (by remi38 on 09/06/2016).
+//  Copyright (c) 2016. All rights reserved.
+//
+
+
+
+import UIKit
+
+extension UIColor {
+    class func hllDarkSkyBlueColor() -> UIColor {
+        return UIColor(red: 81.0/255.0, green: 166.0 / 255.0, blue: 231.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllSkyBlueColor() -> UIColor {
+        return UIColor(red: 124.0/255.0, green: 198.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllBrownishGreyColor() -> UIColor {
+        return UIColor(white: 105.0/255.0, alpha: 1)
+    }
+    
+    class func hllBlackColor() -> UIColor {
+        return UIColor(white: 0.0, alpha: 1)
+    }
+    
+    class func hllWarmGreyColor() -> UIColor {
+        return UIColor(white: 151.0/255.0, alpha: 1)
+    }
+    
+    class func hllWhiteColor() -> UIColor {
+        return UIColor(white: 255.0/255.0, alpha: 1)
+    }
+    
+    class func hllBrownishOrangeColor() -> UIColor {
+        return UIColor(red: 201.0/255.0, green: 85.0 / 255.0, blue: 27.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllMaizeColor() -> UIColor {
+        return UIColor(red: 243.0/255.0, green: 182.0 / 255.0, blue: 90.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllLightTealColor() -> UIColor {
+        return UIColor(red: 125.0/255.0, green: 199.0 / 255.0, blue: 226.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllCoolGreyColor() -> UIColor {
+        return UIColor(red: 158.0/255.0, green: 161.0 / 255.0, blue: 169.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllCharcoalGreyColor() -> UIColor {
+        return UIColor(red: 42.0/255.0, green: 42.0 / 255.0, blue: 51.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllSilverColor() -> UIColor {
+        return UIColor(red: 201.0/255.0, green: 208.0 / 255.0, blue: 216.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllSlateGreyColor() -> UIColor {
+        return UIColor(red: 105.0/255.0, green: 105.0 / 255.0, blue: 122.0 / 255.0, alpha: 1)
+    }
+    
+    class func hllTwilightBlueColor() -> UIColor { 
+        return UIColor(red: 14.0/255.0, green: 46.0 / 255.0, blue: 87.0 / 255.0, alpha: 1)
+    }
+}
